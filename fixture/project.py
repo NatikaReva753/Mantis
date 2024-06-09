3b03c4ad62a0787f8a2bd7f821f76c3862921c21
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-06-09 13:06:46 +0300

    Д/з 25

--- fixture/project.py
+++ fixture/project.py
@@ -12,22 +12,27 @@ class ProjectHelper:
         wd = self.app.wd
         if not (wd.current_url.endswith("/manage_proj_page.php") and len(
                 wd.find_elements_by_xpath("//button[@type='submit']")) > 0):
-            wd.find_element_by_css_selector("i.menu-icon.fa.fa-gears").click()
-            wd.find_element_by_link_text(u"Управление проектами").click()
+            wd.find_element_by_link_text("Manage").click()
+            wd.find_element_by_link_text("Manage Projects").click()
 
     def create(self, project):
         wd = self.app.wd
         self.open_project_page()
         # init group creation
-        wd.find_element_by_xpath("//button[@type='submit']").click()
+        wd.find_element_by_xpath("//input[@value='Create New Project']").click()
         self.fill_project_form(project)
         # submit group creation
-        wd.find_element_by_xpath(u"//input[@value='Добавить проект']").click()
+        wd.find_element_by_xpath("//input[@value='Add Project']").click()
         self.return_to_project_page()
         self.project_cache = None
 
     def fill_project_form(self, project):
+        wd = self.app.wd
+        wd.find_element_by_name("name").click()
+        wd.find_element_by_name("name").clear()
         self.change_field_value("name", project.name)
+        wd.find_element_by_name("description").click()
+        wd.find_element_by_name("description").clear()
         self.change_field_value("description", project.description)
 
     def change_field_value(self, field_name, text):
@@ -62,8 +67,8 @@ class ProjectHelper:
         self.open_project_page()
         self.select_project_by_id(id)
         # submit deletion
-        wd.find_element_by_xpath(u"//input[@value='Удалить проект']").click()
-        wd.find_element_by_xpath(u"//input[@value='Удалить проект']").click()
+        wd.find_element_by_xpath("//input[@value='Delete Project']").click()
+        wd.find_element_by_xpath("//input[@value='Delete Project']").click()
         self.project_cache = None
 
     def select_project_by_id(self, id):
--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,9 @@ from fixture.application import Application
 import pytest
 import json
 import os.path
+import importlib
+import jsonpickle
+from fixture.db import DbFixture
 
 fixture = None
 target = None
@@ -20,10 +23,22 @@ def load_config(file):
 def app(request):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))['web']
+    web_url = load_config(request.config.getoption("--target"))["web"]
+    web_config = load_config(request.config.getoption("--target"))['webadmin']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_URL=web_config['baseURL'])
+        fixture = Application(browser=browser, base_url=web_url["baseUrl"])
+    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
+
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))['db']
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -34,7 +49,30 @@ def stop(request):
     request.addfinalizer(fin)
     return fixture
 
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
+
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
